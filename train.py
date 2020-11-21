820c0af059e1c51c1f7289be090b9efcb8a5e680
SwastiKh <swastikhurana0800@gmail.com>
2020-11-21 08:17:57 +0530

    reverted changes in train.py

--- train.py
+++ train.py
@@ -1,12 +1,5 @@
 import config
 import trainer_step
-import tensorflow
-from tensorflow.compat.v1 import ConfigProto
-from tensorflow.compat.v1 import InteractiveSession
-
-config = ConfigProto()
-config.gpu_options.allow_growth = True
-session = InteractiveSession(config=config)
 
 
 def main():
